2421e947ed4701471a56394ec40bb4ac4c84b4fc
cierpowicz <parylpiotr@gmail.com>
2024-08-21 19:12:26 +0200

    siema

--- src/ultrasensor_class.py
+++ src/ultrasensor_class.py
@@ -14,10 +14,11 @@ class UltraSensor:
         self.trig = 18
         self.echo = 16
         self.i = 1
+
         
-        GPIO.output(self.trig, False)
         GPIO.setup(self.trig,GPIO.OUT)
         GPIO.setup(self.echo,GPIO.IN)
+
 
         
 
@@ -25,29 +26,39 @@ class UltraSensor:
 
     def return_value(self):
 
+        GPIO.output(TRIG, False)
+        print ("Calibrating.....")
+        time.sleep(2)
+
+        print ("Place the object......")
+
         try:
 
-            GPIO.output(self.trig, False)
-            print ("Calibrating.....")
-            time.sleep(2)
+            while True:
+                GPIO.output(self.trig, True)
+                time.sleep(0.00001)
+                GPIO.output(self.trig, False)
 
-            GPIO.output(self.trig, True)
-            time.sleep(0.00001)
-            GPIO.output(self.trig, False)
+                while GPIO.input(self.echo)==0:
+                    pulse_start = time.time()
 
-            while GPIO.input(self.echo)==0:
-                pulse_start = time.time()
+                while GPIO.input(self.echo)==1:
+                    pulse_end = time.time()
 
-            while GPIO.input(self.echo)==1:
-                pulse_end = time.time()
+                pulse_duration = pulse_end - pulse_start
 
-            pulse_duration = pulse_end - pulse_start
+                distance = pulse_duration * 17150
 
-            distance = pulse_duration * 17150
-
-            distance = round(distance+1.15, 2)
-
-            return distance
+                distance = round(distance+1.15, 2)
+            
+                if distance<=20 and distance>=5:
+                    print ("distance:",distance,"cm")
+                    i=1
+                    
+                if distance>20 and i==1:
+                    print ("place the object....")
+                    i=0
+                time.sleep(2)
         
         except:
             print("Problem")
