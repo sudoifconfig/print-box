29896ba4fc60961073a3a94b87bdf0b673f93bda
cierpowicz <parylpiotr@gmail.com>
2024-08-22 17:12:48 +0200

    siema

--- led_test.py
+++ led_test.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+import src.led_class
+
+"""
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(8,GPIO.OUT)
@@ -7,4 +10,10 @@ print ("LED on")
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 print ("LED off")
-GPIO.output(8,GPIO.LOW)
+GPIO.output(8,GPIO.LOW)"""
+
+
+led_01 = src.led_class.LEDclass()
+
+
+led_01.red_led_ON
